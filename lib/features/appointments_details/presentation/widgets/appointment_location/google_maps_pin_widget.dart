9b08c69d4f18a4065cb3b03230efdbcacce575bb
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-06-19 16:52:33 +0300

    custom maps pin or marker done

--- lib/features/appointments_details/presentation/widgets/appointment_location/google_maps_pin_widget.dart
+++ lib/features/appointments_details/presentation/widgets/appointment_location/google_maps_pin_widget.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:medical_appointments/config/theme/color_manager/colors.dart';
-import 'package:medical_appointments/core/constants/app_borders.dart';
-import 'package:medical_appointments/core/constants/app_paddings.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../../../../config/theme/color_manager/colors.dart';
+import '../../../../../core/constants/app_borders.dart';
+import '../../../../../core/constants/app_images.dart';
+import '../../../../../core/constants/app_paddings.dart';
 import '../../../../../core/constants/app_sizes.dart';
 
 class GoogleMapsPinWidget extends StatelessWidget
@@ -31,4 +33,30 @@ class GoogleMapsPinWidget extends StatelessWidget
       ),
     );
   }
+}
+
+
+class GoogleMapsPinWidget2
+{
+  final BitmapDescriptor bitmapDescriptor;
+  final Image pinImage;
+
+  GoogleMapsPinWidget2({
+    required this.bitmapDescriptor,
+    required this.pinImage,
+  });
+}
+
+
+Future<GoogleMapsPinWidget2> customPinWidget() async
+{
+  // 1. Create Google Maps marker from asset image
+  final BitmapDescriptor markerBitmap = await BitmapDescriptor.asset(const ImageConfiguration(size: Size(72, 72)), AppAssets.icons.googleMapsPin,);
+
+  // 2. Load Image widget (for UI display)
+  final Image pinImage = Image.asset(AppAssets.icons.googleMapsPin, width: 72, height: 72);
+  return GoogleMapsPinWidget2(
+    bitmapDescriptor: markerBitmap,
+    pinImage: pinImage,
+  );
 }
--- lib/features/appointments_details/presentation/widgets/appointment_location/google_maps_widget.dart
+++ lib/features/appointments_details/presentation/widgets/appointment_location/google_maps_widget.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_local_variable
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -9,6 +7,7 @@ import '../../../../../config/theme/color_manager/colors.dart';
 import '../../../../../core/constants/app_borders.dart';
 import '../../../../../core/utils/url_launcher.dart';
 import 'google_maps_get_direction_widget.dart';
+import 'google_maps_pin_widget.dart';
 
 class GoogleMapsWidget extends StatelessWidget
 {
@@ -20,13 +19,15 @@ class GoogleMapsWidget extends StatelessWidget
     const LatLng clinicLocation = LatLng(24.7136, 46.6753);
     const CameraPosition initialCameraPosition = CameraPosition(target: clinicLocation, zoom: 15.0,);
     return FutureBuilder(
-      future: _createCustomMarker(),
+      future: customPinWidget(),
       builder: (context, snapshot)
       {
         if (!snapshot.hasData)
         {
           return const AppCircularIndicator();
         }
+
+        final markerData = snapshot.data!;
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children:
@@ -45,16 +46,18 @@ class GoogleMapsWidget extends StatelessWidget
                         Marker(
                           markerId: const MarkerId('clinic_location'),
                           position: clinicLocation,
-                          icon: snapshot.data!,
+                          icon: markerData.bitmapDescriptor,
                           anchor: const Offset(0.5, 0.5),
                         ),
                       },
-                      zoomControlsEnabled: false, myLocationEnabled: true, //mapType: MapType.normal,
+                      zoomControlsEnabled: false,
+                      myLocationEnabled: true,
                     ),
-                    Positioned(bottom: 8.h, right: 12.w, 
+                    Positioned(bottom: 8.h, right: 12.w,
                       child: GestureDetector(
                         onTap: () => launchMapsUrl(clinicLocation),
-                        child: const GetDirectionsWidget()),
+                        child: const GetDirectionsWidget(),
+                      ),
                     ),
                   ],
                 ),
@@ -66,16 +69,3 @@ class GoogleMapsWidget extends StatelessWidget
     );
   }
 }
-
-Future<BitmapDescriptor> _createCustomMarker() async
-  {
-    // Create a canvas to draw the custom marker
-    final PictureRecorder recorder = PictureRecorder();
-    final Canvas canvas = Canvas(recorder);
-
-    // Convert to image and then to BitmapDescriptor
-    final picture = recorder.endRecording();
-    final image = await picture.toImage(72, 72);
-    final bytes = await image.toByteData(format: ImageByteFormat.png);
-    return BitmapDescriptor.bytes(bytes!.buffer.asUint8List());
-  }
